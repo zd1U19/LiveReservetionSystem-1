35c1b7dcfe352cd9b9c7b40b0ecdd2e91e185845
zd1U19 <bmayu.java@gmail.com>
2025-08-20 11:54:15 +0900

    管理者機能完成

--- src/main/java/com/example/app/config/ApplicationConfig.java
+++ src/main/java/com/example/app/config/ApplicationConfig.java
@@ -1,25 +1,41 @@
 package com.example.app.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class ApplicationConfig implements WebMvcConfigurer{
-	
-	 // バリデーションメッセージのカスタマイズ
-	 @Override
-	 public Validator getValidator() {
-	 var validator = new LocalValidatorFactoryBean();
-	 validator.setValidationMessageSource(messageSource());
-	 return validator;
-	 }
-	 @Bean
-	 ResourceBundleMessageSource messageSource() {
-	 var messageSource = new ResourceBundleMessageSource();
-	 messageSource.setBasename("validation");
-	 return messageSource;
-	 }
+import com.example.app.filter.AuthFilter;
+
+@Configuration
+public class ApplicationConfig implements WebMvcConfigurer {
+
+	// バリデーションメッセージのカスタマイズ
+	@Override
+	public Validator getValidator() {
+		var validator = new LocalValidatorFactoryBean();
+		validator.setValidationMessageSource(messageSource());
+		return validator;
+	}
+
+	@Bean
+	ResourceBundleMessageSource messageSource() {
+		var messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("validation");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	// 認証用フィルタの有効化
+	@Bean
+	FilterRegistrationBean<AuthFilter> authFilter() {
+		var bean = new FilterRegistrationBean<AuthFilter>(new AuthFilter());
+		bean.addUrlPatterns("/members/*");
+		bean.addUrlPatterns("/news/*");
+		return bean;
+	}
 
 }
--- src/main/java/com/example/app/controller/AuthController.java
+++ src/main/java/com/example/app/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.app.domain.Admin;
@@ -29,7 +30,7 @@ public class AuthController {
 
 	@PostMapping("/admin/home")
 	public String login(
-			@Valid Admin admin,
+			@Valid @ModelAttribute("admin")Admin admin,
 			Errors errors,
 			Model model) {
 		// 入力に不備がある
